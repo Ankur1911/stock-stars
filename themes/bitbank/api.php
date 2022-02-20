1470c62e3015d23699719678f70885943fff86ff
Ankur1911 <ankurkapuriya2002@gmail.com>
2022-02-20 10:31:50 +0530

    first

---
+++ themes/bitbank/api.php
@@ -0,0 +1,42 @@
+<?php
+<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+{
+    "symbols": [
+    {
+    "description": "SENSEX",
+    "proName": "BSE:SENSEX"
+    },
+    {
+    "description": "RELIANCE",
+    "proName": "BSE:RELIANCE"
+    },
+    {
+    "description": "INFY",
+    "proName": "BSE:INFY"
+    },
+    {
+    "description": "TCS",
+    "proName": "BSE:TCS"
+    },
+    {
+    "description": "WIPRO",
+    "proName": "BSE:WIPRO"
+    },
+    {
+    "description": "TATA POWER",
+    "proName": "BSE:TATAPOWER"
+    },
+    {
+    "description": "SUZLON",
+    "proName": "BSE:SUZLON"
+    }
+
+    ],
+    "showSymbolLogo": true,
+    "colorTheme": "dark",
+    "isTransparent": false,
+    "displayMode": "regular",
+    "locale": "in"
+    }
+    </script>
+?>
