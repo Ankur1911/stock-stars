bb783753b7e79bb0728263ec25d1b905713d32eb
Ankur1911 <ankurkapuriya2002@gmail.com>
2022-03-02 15:31:04 +0530

    header

--- themes/bitbank/login.php
+++ themes/bitbank/login.php
@@ -98,56 +98,7 @@ Fixed Navigation
 ==================================== -->
 
 <?php include 'api.php' ?>
-<section class="header  navigation">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg">
-                    <a class="navbar-brand" href="index.php">
-                        <img src="images/logo.png" alt="logo">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="tf-ion-android-menu"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="pricing.php">Buy/Sell</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="faq.php">F.A.Q</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Contact</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="login.php">Sign In</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="signup.php">Sign Up</a>
-                            </li>
-
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Pages
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="team.php">Team Page</a>
-                                    <a class="dropdown-item" href="404.php">404 Page</a>
-                                    <a class="dropdown-item" href="blog.php">Blog Page</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                
-            </div>
-        </div>
-    </div>
-</section>
+<?php include 'header.php' ?>
 <section class="signin-page account">
     <div class="container">
         <div class="row">
