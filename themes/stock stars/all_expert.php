fe3a74bfc23d4703467929db2ba62205342b2606
vivek <vivek.kakadiya111@gmail.com>
2022-04-14 13:29:55 +0530

    ..

--- themes/stock stars/all_expert.php
+++ themes/stock stars/all_expert.php
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -44,11 +53,10 @@
     
  </style>
 
+
 <body id="body"> 
 <?php include 'particle/header2.php' ?>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
 
 
@@ -77,49 +85,6 @@
     </div>';
   }
   
-  
-  
-  
-?>
-=======
-<div class="card" style="width: 18rem; float:left;">
-  <img src="images/client-logo/clients-1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
->>>>>>> 13059b519422155b1612b5bb436e6cfd72f8c05e
-=======
-<?php
->>>>>>> 760ff049add64b0cb7e5b9616107088134f51a49
-
-
-    include 'particle/_dbconn.php';
-
-    $sql="select * from expert";
-    $result = mysqli_query($conn, $sql); 
-    $rec = mysqli_fetch_assoc($result);
-  
-  
-  while($rec = mysqli_fetch_assoc($result)){
-
-
-    
-
-    $name=$rec['FName'];
-    $dec=$rec['Expertise'];
-  
-    echo '<div class="card" style="width: 18rem;">
-      <img src="images/client-logo/clients-1.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">'.$name.'</h5>
-        <p class="card-text">'.$dec.'</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>';
-  }
   
   
   
@@ -127,4 +92,6 @@
 ?>
 
 </body>
+
+
 </html>
