7c77a8526d3717415a72e1d38f7f22fac3cca66a
Ankur1911 <ankurkapuriya2002@gmail.com>
2022-04-14 12:43:04 +0530

    //

---
+++ themes/stock stars/expert/all expert.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html lang="zxx"> <!--<![endif]-->
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Bingo One page parallax responsive HTML Template ">
+  
+  <meta name="author" content="stock stars.com">
+
+
+  
+  <title>Stock Stars</title>
+
+<!-- Mobile Specific Meta
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+  
+  <!-- CSS
+  ================================================== -->
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="plugins/themefisher-font.v-2/style.css">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Slick Carousel -->
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+  
+
+</head>
+<style>
+    .hero-area{
+ background-image: url('images/slider/bg.jpg');
+    }
+    
+ </style>
+
+<body id="body"> 
+    <section class="header  navigation">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="index.php">
+                            <img src="images/LOGO.png" alt="logo">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="tf-ion-android-menu"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <!-- <li class="nav-item">
+                                    <a class="nav-link" href="pricing.php">Buy/Sell</a>
+                                </li> -->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="faq.php">F.A.Q</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="login.php">Sign In</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="signup.php">Sign Up</a>
+                                </li>
+    
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Pages
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="team.php">Team Page</a>
+                                        <!-- <a class="dropdown-item" href="404.php">404 Page</a> -->
+                                        <!-- <a class="dropdown-item" href="blog.php">Blog Page</a> -->
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+  </html>
