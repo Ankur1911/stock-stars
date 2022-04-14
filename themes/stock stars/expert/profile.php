3d0070bb54f6f205e0f3dfc52bd059df88a77dba
Ankur1911 <ankurkapuriya2002@gmail.com>
2022-04-14 19:48:10 +0530

    ..

--- themes/stock stars/expert/profile.php
+++ themes/stock stars/expert/profile.php
@@ -69,16 +69,16 @@
                     <div class="row">
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="500" data-speed="500">500</h6>
+                                <h6 class="count h2" data-to="500" data-speed="500">#+</h6>
                                 <p class="m-0px font-w-600">Happy Clients</p>
                             </div>
                         </div>
-                        <div class="col-6 col-lg-3">
+                        <!-- <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
                                 <h6 class="count h2" data-to="150" data-speed="150">150</h6>
                                 <p class="m-0px font-w-600">Project Completed</p>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
                                 <h6 class="count h2" data-to="850" data-speed="850">#+</h6>
@@ -87,7 +87,7 @@
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="190" data-speed="190">*+</h6>
+                                <h6 class="count h2" data-to="190" data-speed="190">#+</h6>
                                 <p class="m-0px font-w-600">Telephonic Talk</p>
                             </div>
                         </div>
