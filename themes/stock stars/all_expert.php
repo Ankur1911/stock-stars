63ca74e617796c791be22209ca43b8d0222b6d24
Ankur1911 <ankurkapuriya2002@gmail.com>
2022-04-14 15:32:05 +0530

    ../

--- themes/stock stars/all_expert.php
+++ themes/stock stars/all_expert.php
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -53,7 +44,6 @@
     
  </style>
 
-
 <body id="body"> 
 <?php include 'particle/header2.php' ?>
 
@@ -77,27 +67,21 @@
     $dec=$rec['Expertise'];
     $email=$rec['EmailID'];
   
-    
-    echo '<div class="col-md-4"> <div class="card" style="width: 18rem;">
-      <img src="images/client-logo/clients-1.jpg" class="card-img-top" alt="...">
+    echo '<div class="card" style="width: 18rem; margin:8px; width:250px;height: 450px; float:left;">
+      <img src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">'.$name.'</h5>
         <h6 class="card-title">'.$email.'</h6>
         <p class="card-text">'.$dec.'</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-      </div>
+            </div>
     </div>';
   }
   // <a href="#" class="btn btn-primary">Go somewhere</a>
 
   
   
-
   
 ?>
 
 </body>
-
-
 </html>
