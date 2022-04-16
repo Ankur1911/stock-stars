5b36c1a13bff50b734b9ada92a10e8c3c9cea052
vivek <vivek.kakadiya111@gmail.com>
2022-04-16 10:00:25 +0530

    final

--- themes/stock stars/expert/profile.php
+++ themes/stock stars/expert/profile.php
@@ -1,3 +1,24 @@
+
+
+
+<?php
+
+ include '../particle/_dbconn.php';
+ session_start();
+    
+   
+    $Email = $_SESSION['EEEmail'];
+    
+    
+   
+
+    $sql = "SELECT * FROM `expert` WHERE Email= '$Email';";
+    $result = mysqli_query($conn, $sql);
+		$rec = mysqli_fetch_assoc($result);
+   
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +27,9 @@
     <!--  All snippets are MIT license http://bootdey.com/license -->
     <title>about me - Bootdey.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <section class="section about-section gray-bg" id="about">
@@ -17,43 +38,43 @@
                     <div class="col-lg-6">
                         <div class="about-text go-to">
                             <h3 class="dark-color">About Me</h3>
-                            <h6 class="theme-color lead">A Lead UX &amp; UI designer based in Canada</h6>
-                            <p>I <mark>design and develop</mark> services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface and meaningful interactions.</p>
+                            <!-- <h6 class="theme-color lead">A Lead UX &amp; UI designer based in Canada</h6>
+                            <p>I <mark>design and develop</mark> services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface and meaningful interactions.</p> -->
                             <div class="row about-list">
                                 <div class="col-md-6">
                                     <div class="media">
-                                        <label>Birthday</label>
-                                        <p>4th april 1998</p>
+                                        <label>Name</label>
+                                        <p><?php echo $rec['FName']; ?></p>
                                     </div>
                                     <div class="media">
-                                        <label>Age</label>
-                                        <p>22 Yr</p>
+                                        <label>Phone</label>
+                                        <p><?php echo $rec['MobileNumber']; ?></p>
                                     </div>
                                     <div class="media">
-                                        <label>Residence</label>
-                                        <p>Canada</p>
+                                        <label>Email</label>
+                                        <p><?php echo $rec['Email']; ?></p>
                                     </div>
                                     <div class="media">
-                                        <label>Address</label>
-                                        <p>California, USA</p>
+                                        <label>Experience</label>
+                                        <p><?php echo $rec['Experience']; ?></p>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="media">
-                                        <label>E-mail</label>
-                                        <p>info@domain.com</p>
+                                        <label>Charge/1</label>
+                                        <p><?php echo $rec['Charge1']; ?></p>
                                     </div>
                                     <div class="media">
-                                        <label>Phone</label>
-                                        <p>820-885-3321</p>
+                                        <label>Charge/3</label>
+                                        <p><?php echo $rec['Charge3']; ?></p>
                                     </div>
                                     <div class="media">
-                                        <label>Skype</label>
-                                        <p>skype.0404</p>
+                                        <label>Charge/6</label>
+                                        <p><?php echo $rec['Charge6']; ?></p>
                                     </div>
                                     <div class="media">
-                                        <label>Freelance</label>
-                                        <p>Available</p>
+                                        <label>Charge/12</label>
+                                        <p><?php echo $rec['Charge12']; ?></p>
                                     </div>
                                 </div>
                             </div>
@@ -69,25 +90,25 @@
                     <div class="row">
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="500" data-speed="500">500</h6>
+                                <h6 class="count h2" data-to="500" data-speed="500">#+</h6>
                                 <p class="m-0px font-w-600">Happy Clients</p>
                             </div>
                         </div>
-                        <div class="col-6 col-lg-3">
+                        <!-- <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
                                 <h6 class="count h2" data-to="150" data-speed="150">150</h6>
                                 <p class="m-0px font-w-600">Project Completed</p>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="850" data-speed="850">850</h6>
-                                <p class="m-0px font-w-600">Photo Capture</p>
+                                <h6 class="count h2" data-to="850" data-speed="850">#+</h6>
+                                <p class="m-0px font-w-600">Days of Trading</p>
                             </div>
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="190" data-speed="190">190</h6>
+                                <h6 class="count h2" data-to="190" data-speed="190">#+</h6>
                                 <p class="m-0px font-w-600">Telephonic Talk</p>
                             </div>
                         </div>
