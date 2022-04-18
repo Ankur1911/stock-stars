ebfa82176af4a559ffd9d6b2f36def1d8544fd2c
Ankur1911 <ankurkapuriya2002@gmail.com>
2022-04-18 15:25:36 +0530

    expert form

---
+++ themes/stock stars/expert_valiation_form.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+<style>
+    h1{
+        border:solid blue 2px; 
+    }
+    .form{
+        margin-left: 300px;
+        margin-right: 300px;
+        border: solid black 2px;
+    }
+    h1{
+        margin-left: 420px;
+        margin-right: 410px;
+        padding:10px;
+        border-radius: 20px;
+        background-color: cadetblue;
+
+    }
+    </style>
+</head>
+<body>
+    <div class="header">
+    <h1>Expert Validation Form</h1>
+</div>
+    <form>
+        <div class="form">
+        <p>1. qustion 1</p>
+        <input type="radio" name="q1o" value="q1option1">ans1
+        <input type="radio" name="q1o" value="q1option2">ans1
+        <input type="radio" name="q1o" value="q1option3">ans1
+        <input type="radio" name="q1o" value="q1option4">ans1
+
+        <p>2. qustion 1</p>
+        <input type="radio" name="q2o" value="q2option1">ans1
+        <input type="radio" name="q2o" value="q2option2">ans1
+        <input type="radio" name="q2o" value="q2option3">ans1
+        <input type="radio" name="q2o" value="q2option4">ans1
+
+        <p>3. qustion 1</p>
+        <input type="radio" name="q3o" value="q3option1">ans1
+        <input type="radio" name="q3o" value="q3option2">ans1
+        <input type="radio" name="q3o" value="q3option3">ans1
+        <input type="radio" name="q3o" value="q3option4">ans1
+
+        <p>4. qustion 1</p>
+        <input type="radio" name="q4o" value="q4option1">ans1
+        <input type="radio" name="q4o" value="q4option2">ans1
+        <input type="radio" name="q4o" value="q4option3">ans1
+        <input type="radio" name="q4o" value="q4option4">ans1
+
+        <p>5. qustion 1</p>
+        <input type="radio" name="q5o" value="q5option1">ans1
+        <input type="radio" name="q5o" value="q5option2">ans1
+        <input type="radio" name="q5o" value="q5option3">ans1
+        <input type="radio" name="q5o" value="q5option4">ans1
+
+        <p>6. qustion 1</p>
+        <input type="radio" name="q6o" value="q6option1">ans1
+        <input type="radio" name="q6o" value="q6option2">ans1
+        <input type="radio" name="q6o" value="q6option3">ans1
+        <input type="radio" name="q6o" value="q6option4">ans1
+
+        <p>7. qustion 1</p>
+        <input type="radio" name="q7o" value="q7option1">ans1
+        <input type="radio" name="q7o" value="q7option2">ans1
+        <input type="radio" name="q7o" value="q7option3">ans1
+        <input type="radio" name="q7o" value="q7option4">ans1
+
+        <p>8. qustion 1</p>
+        <input type="radio" name="q8o" value="q8option1">ans1
+        <input type="radio" name="q8o" value="q8option2">ans1
+        <input type="radio" name="q8o" value="q8option3">ans1
+        <input type="radio" name="q8o" value="q8option4">ans1
+
+        <p>9. qustion 1</p>
+        <input type="radio" name="q9o" value="q9option1">ans1
+        <input type="radio" name="q9o" value="q9option2">ans1
+        <input type="radio" name="q9o" value="q9option3">ans1
+        <input type="radio" name="q9o" value="q9option4">ans1
+
+        <p>10. qustion 1</p>
+        <input type="radio" name="q10o" value="q10option1">ans1
+        <input type="radio" name="q10o" value="q10option2">ans1
+        <input type="radio" name="q10o" value="q10option3">ans1
+        <input type="radio" name="q10o" value="q10option4">ans1
+    </div>
+    </form>
+
+</body>
+</html>
